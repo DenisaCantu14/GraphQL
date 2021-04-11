8954bed77575ef1ddcfd178d2738be61d8e864d3
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-04-11 15:04:08 +0300

    creating a schema

---
+++ index.js
@@ -0,0 +1,44 @@
+const {ApolloServer, gql} = require('apollo-server')
+const SessionAPI = require('./datasources/sessions')
+
+
+const typeDefs = gql`
+type Query{
+    sessions:[Session],
+    sessionById(id:ID): Session
+}
+type Session
+{
+    id :ID!,
+    title: String!,
+    description: String,
+    startsAt: String,
+    endsAt: String,
+    room:String,
+    day:String,
+    format:String, 
+    track:String @deprecated(reason: "To many..." ),
+    level:String
+}`
+
+const resolvers = {
+    Query: {
+        sessions:(parent, args, {dataSources}, info) => {
+            return dataSources.sessionAPI.getSessions();}
+        ,
+        sessionById:(parent, {id}, {dataSources}, info) => {
+            return dataSources.sessionAPI.getSessionById(id);
+        }  
+    }   
+}
+
+const dataSources = () => ({
+    sessionAPI: new SessionAPI ()
+})
+
+const server = new ApolloServer({typeDefs, resolvers, dataSources});
+server
+    .listen({port: process.env.PORT || 4000})
+    .then (({url}) => {
+        console.log(`graphQL RUNNING AT ${url}`);
+    })
